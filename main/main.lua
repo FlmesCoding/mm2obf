0eddc768ffbc273a8cf45d178b7cea02e72d52ef
Flames <134011556+FlmesCoding@users.noreply.github.com>
2023-07-24 09:15:51 -0400

    Create main.lua

---
+++ main/main.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 142823291 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/FlmesCoding/mm2obf/main/mm2.lua"))()
+else
+	if game.PlaceId == 537413528 then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/FlmesCoding/mm2obf/main/babft.lua"))()
+  end
