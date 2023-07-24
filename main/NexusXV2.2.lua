5d2b2c98f6fe04f80f27dd7e0f1c89585864723b
Flames <134011556+FlmesCoding@users.noreply.github.com>
2023-07-24 09:20:24 -0400

    Update and rename NexusXV2.2.lua to NexusXV2.3.lua

--- main/NexusXV2.2.lua
+++
@@ -1,1 +0,0 @@
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/FlmesCoding/mm2obf/main/main/main.lua"))()
---
+++ main/NexusXV2.3.lua
@@ -0,0 +1,1 @@
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/FlmesCoding/mm2obf/main/main/Main2.lua"))()
